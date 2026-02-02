ce2fa968a82c0c072a1a872feb6b5bb0c3b24c55
milantbh <75333500+milantbh@users.noreply.github.com>
2026-02-02 17:06:47 +0100

    fix bug

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,6 @@ const Login = ({ setToken }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const endpoint = isRegistering ? '/register' : '/login';
 
 const API_BASE = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
 
